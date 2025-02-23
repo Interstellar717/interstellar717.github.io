87fa0327889e7931612955850722ec021b313315
Interstellar717 <obsidianflames846@gmail.com>
2025-02-23 11:00:00 -0500

    change

---
+++ assets/scripts/script.js
@@ -0,0 +1,3 @@
+const qs = i => { return document.querySelector(i); }
+const qsa = i => { return document.querySelectorAll(i); }
+const log = i => { console.log(i); }
---
+++ assets/scripts/size.js
@@ -0,0 +1,6 @@
+/* qs('html').style.height = (window.innerHeight - 30) + 'px';
+qs('html').style.width = (window.innerWidth - 30) + 'px';
+qs('body').style.height = (window.innerHeight - 30) + 'px';
+qs('body').style.width = (window.innerWidth - 30) + 'px';
+qs('.content').style.height = (window.innerHeight - 30) + 'px';
+qs('.content').style.width = (window.innerWidth - 30) + 'px'; */
